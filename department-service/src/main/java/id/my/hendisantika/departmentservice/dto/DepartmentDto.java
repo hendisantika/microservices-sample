8ba3aefc6f3e01363dc0f1baf6110ec983da7fb5
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 09:44:01 +0700

    Add DepartmentDto🫸🌀✏️📗🐧🐳

---
+++ department-service/src/main/java/id/my/hendisantika/departmentservice/dto/DepartmentDto.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.departmentservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/20/24
+ * Time: 09:43
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DepartmentDto {
+    private String departmentName;
+    private String departmentDescription;
+    private String departmentCode;
+}
