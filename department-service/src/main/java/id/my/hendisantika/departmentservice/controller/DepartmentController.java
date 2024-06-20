e7d232e8ccb1c4d73a8ad16299cbb40db5f3caf7
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 09:46:33 +0700

    Add getDepartmentByCode in DepartmentController🫸🌀✏️📗🐧🐳

--- department-service/src/main/java/id/my/hendisantika/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/id/my/hendisantika/departmentservice/controller/DepartmentController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +35,13 @@ public class DepartmentController {
         DepartmentDto savDepartment = departmentService.savDepartment(departmentDto);
 
         return new ResponseEntity<>(savDepartment, HttpStatus.CREATED);
+    }
+
+    // Build Get Department By ID Rest API
+    @GetMapping("{department-code}")
+    public ResponseEntity<DepartmentDto> getDepartmentByCode(@PathVariable("department-code") String code) {
+        DepartmentDto department = departmentService.findByDepartmentCode(code);
+
+        return new ResponseEntity<>(department, HttpStatus.OK);
     }
 }
