31f62711befebf1a46f1112f4532fd96e25e461e
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 09:42:36 +0700

    Add Discovery Client🫸🌀✏️📗🐧🐳

--- department-service/src/main/java/id/my/hendisantika/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/id/my/hendisantika/departmentservice/DepartmentServiceApplication.java
@@ -1,13 +1,21 @@
 package id.my.hendisantika.departmentservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DepartmentServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DepartmentServiceApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
