486c125ad7acb7d0654a4e3aa2fbf75b816d84dc
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 09:35:16 +0700

    EnableFeignClients🫸🌀✏️📗🐧🐳

--- employee-service/src/main/java/id/my/hendisantika/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/id/my/hendisantika/employeeservice/EmployeeServiceApplication.java
@@ -1,13 +1,36 @@
 package id.my.hendisantika.employeeservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
     }
 
 }
