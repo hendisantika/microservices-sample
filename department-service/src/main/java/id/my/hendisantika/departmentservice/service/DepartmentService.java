ea16e02c0072b0c2b56394e5250cbda98f9f65fd
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 09:44:51 +0700

    Add DepartmentService🫸🌀✏️📗🐧🐳

---
+++ department-service/src/main/java/id/my/hendisantika/departmentservice/service/DepartmentService.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.departmentservice.service;
+
+import id.my.hendisantika.departmentservice.repository.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/20/24
+ * Time: 09:44
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DepartmentService {
+    private final DepartmentRepository departmentRepository;
+
+    private final ModelMapper modelMapper;
+
+}
