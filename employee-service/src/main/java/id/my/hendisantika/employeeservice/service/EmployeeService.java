2bce1ef5d51568c5c836317b08c1b65e9768a865
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 09:32:39 +0700

    Add EmployeeService🫸🌀✏️📗🐧🐳

---
+++ employee-service/src/main/java/id/my/hendisantika/employeeservice/service/EmployeeService.java
@@ -0,0 +1,34 @@
+package id.my.hendisantika.employeeservice.service;
+
+import id.my.hendisantika.employeeservice.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/20/24
+ * Time: 09:32
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    private final ModelMapper modelMapper;
+
+    private final RestTemplate restTemplate;
+
+    private final WebClient webClient;
+
+    private final APIClient apiClient;
+}
