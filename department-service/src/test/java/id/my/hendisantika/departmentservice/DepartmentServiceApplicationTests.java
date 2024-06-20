eedf02fa5fac73ebab6eb0697cf47d665647ebde
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 09:38:59 +0700

    Add department-service🫸🌀✏️📗🐧🐳

---
+++ department-service/src/test/java/id/my/hendisantika/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
