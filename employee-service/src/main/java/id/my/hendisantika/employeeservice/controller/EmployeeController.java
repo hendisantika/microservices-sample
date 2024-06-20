4fa4a70cd43a55ddd1ea7c430adde1a66e9478e2
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 09:36:32 +0700

    Add EmployeeController🫸🌀✏️📗🐧🐳

---
+++ employee-service/src/main/java/id/my/hendisantika/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.employeeservice.controller;
+
+import id.my.hendisantika.employeeservice.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/20/24
+ * Time: 09:36
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/employees")
+@RefreshScope
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+    @Value("${spring.app.message}")
+    String message;
+
+
+}
